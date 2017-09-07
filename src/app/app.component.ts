396cb585e5cebfc6401a57e8fe6e816b3a3d27d0
White, Durward P <durward.white@enablemidstream.com>
2017-09-07 17:36:25 -0500

    Added new routes and css3 styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+    selector: 'my-app',
+    templateUrl: './index.component.html'
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent {
+    title = 'White for State House 28'
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent }  from './app.component';
+import { AboutComponent } from './about.component';
+import { IndexComponent } from './index.component';
+import { HomeComponent } from './home.component';
+
+const routes: Routes = [
+  {path: 'about', component: AboutComponent},
+  {path: 'home', component: HomeComponent}
+];
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, RouterModule.forRoot(routes) ],
+  declarations: [ AppComponent, IndexComponent, AboutComponent, HomeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
